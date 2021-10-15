416b01d94ad2f6b6d90aacd54a58c7eef45b1c23
Josué Munganga <jmunganga039@gmail.com>
2021-10-15 14:14:11 +0200

    Worked on Announcements & Slider components

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/P9JKhMk/Seek-Png-com-black-girl-png-784529.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/P9JKhMk/Seek-Png-com-black-girl-png-784529.png",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL.",
+        bg: "f5f1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/P9JKhMk/Seek-Png-com-black-girl-png-784529.png",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL.",
+        bg: "fbf0f4",
+    },
+]
