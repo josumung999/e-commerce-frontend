58d2f364f28d43e7f78f6fd25d8402272f80a193
Josué Munganga <jmunganga039@gmail.com>
2021-10-19 12:33:23 +0200

    Worked on the Single Product, Finished the ProductList and Created the Login and Register Page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+//import ProductList from "./pages/ProductList";
+
+import Login from "./pages/Login";
+
+//import Register from "./pages/Register";
+
+//import Product from "./pages/Product";
 
 const App = () => {
-  return <Home />
+  return <Login />
 };
 
 export default App;
